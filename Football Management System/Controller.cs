e48c4836f87926f20d1c508ecfbc41d84384c0aa
Omar Hadhoud <OmarHadhoud@ymail.com>
2019-12-11 16:04:21 +0200

    Added DBManager, Controller and Stored procedures files.

---
+++ Football Management System/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Windows.Forms;
+
+namespace DBapplication
+{
+    public class Controller
+    {
+        DBManager dbMan;
+        public Controller()
+        {
+            dbMan = new DBManager();
+        }
+
+      
+        public void TerminateConnection()
+        {
+            dbMan.CloseConnection();
+        }
+        
+    }
+}
