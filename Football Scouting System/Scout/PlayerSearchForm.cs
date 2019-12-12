2d761bd19df90248207186cf3562e50ba1dbc88f
Omar Fasseh <okomarkhaled@gmail.com>
2019-12-13 01:54:10 +0200

    Basic scouts

---
+++ Football Scouting System/Scout/PlayerSearchForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Football_Scouting_System.Scout
+{
+    public partial class PlayerSearchForm : Form
+    {
+        Form ParentForm_;
+        public PlayerSearchForm(Form _ParentForm)
+        {
+            InitializeComponent();
+            ParentForm_ = _ParentForm;
+        }
+
+        private void PlayerSearchForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ParentForm_.Show();
+        }
+    }
+}
