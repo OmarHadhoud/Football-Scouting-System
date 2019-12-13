1bd74d520acd0cbda4dd8c1eafe39ddd3de6801a
OmarFasseh <48892906+OmarFasseh@users.noreply.github.com>
2019-12-13 16:39:10 +0200

    Update Order_search_PlayerForScouts.sql

--- StoredProcedures/Order_search_PlayerForScouts.sql
+++ StoredProcedures/Order_search_PlayerForScouts.sql
@@ -1,3 +1,14 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -13,7 +24,11 @@ Create PROCEDURE Order_search_PlayerForScouts
 	@inputN varchar(50)	
 	AS
 BEGIN
+-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
 
+    -- Insert statements for procedure here
 
 select [Fname]
       ,[Lname]
