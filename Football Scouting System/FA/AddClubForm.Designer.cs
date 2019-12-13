75e65039f682b67530e9b3bcfcf8db23e2496d0d
Omar Hadhoud <OmarHadhoud@ymail.com>
2019-12-13 10:06:38 +0200

    Implemented the Add club functionality.

--- Football Scouting System/FA/AddClubForm.Designer.cs
+++ Football Scouting System/FA/AddClubForm.Designer.cs
@@ -30,13 +30,26 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.clubIdTxtBox = new System.Windows.Forms.TextBox();
+            this.clubNameTxtBox = new System.Windows.Forms.TextBox();
+            this.clubAbbvTxtBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.foundDate = new System.Windows.Forms.DateTimePicker();
+            this.clubPassTxtBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.clubCityTxtBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(28, 62);
+            this.label1.Location = new System.Drawing.Point(28, 97);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(126, 25);
             this.label1.TabIndex = 0;
@@ -46,19 +59,150 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(28, 112);
+            this.label2.Location = new System.Drawing.Point(28, 48);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(91, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Club ID:";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Club Abbreviation:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Club League:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(28, 319);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(229, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Club Foundation Date:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(28, 374);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(164, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Club Password:";
+            // 
+            // clubIdTxtBox
+            // 
+            this.clubIdTxtBox.Location = new System.Drawing.Point(157, 48);
+            this.clubIdTxtBox.Name = "clubIdTxtBox";
+            this.clubIdTxtBox.Size = new System.Drawing.Size(119, 22);
+            this.clubIdTxtBox.TabIndex = 6;
+            // 
+            // clubNameTxtBox
+            // 
+            this.clubNameTxtBox.Location = new System.Drawing.Point(157, 97);
+            this.clubNameTxtBox.MaxLength = 50;
+            this.clubNameTxtBox.Name = "clubNameTxtBox";
+            this.clubNameTxtBox.Size = new System.Drawing.Size(119, 22);
+            this.clubNameTxtBox.TabIndex = 7;
+            // 
+            // clubAbbvTxtBox
+            // 
+            this.clubAbbvTxtBox.Location = new System.Drawing.Point(236, 153);
+            this.clubAbbvTxtBox.MaxLength = 3;
+            this.clubAbbvTxtBox.Name = "clubAbbvTxtBox";
+            this.clubAbbvTxtBox.Size = new System.Drawing.Size(66, 22);
+            this.clubAbbvTxtBox.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(181, 262);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // foundDate
+            // 
+            this.foundDate.Location = new System.Drawing.Point(268, 321);
+            this.foundDate.Name = "foundDate";
+            this.foundDate.Size = new System.Drawing.Size(247, 22);
+            this.foundDate.TabIndex = 10;
+            // 
+            // clubPassTxtBox
+            // 
+            this.clubPassTxtBox.Location = new System.Drawing.Point(205, 376);
+            this.clubPassTxtBox.MaxLength = 50;
+            this.clubPassTxtBox.Name = "clubPassTxtBox";
+            this.clubPassTxtBox.PasswordChar = '*';
+            this.clubPassTxtBox.Size = new System.Drawing.Size(135, 22);
+            this.clubPassTxtBox.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(597, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 262);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Add Club";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // clubCityTxtBox
+            // 
+            this.clubCityTxtBox.Location = new System.Drawing.Point(153, 208);
+            this.clubCityTxtBox.MaxLength = 50;
+            this.clubCityTxtBox.Name = "clubCityTxtBox";
+            this.clubCityTxtBox.Size = new System.Drawing.Size(104, 22);
+            this.clubCityTxtBox.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(28, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Club City:";
+            // 
             // AddClubForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clubCityTxtBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clubPassTxtBox);
+            this.Controls.Add(this.foundDate);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.clubAbbvTxtBox);
+            this.Controls.Add(this.clubNameTxtBox);
+            this.Controls.Add(this.clubIdTxtBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "AddClubForm";
             this.Text = "AddClubForm";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AddClubForm_FormClosed);
@@ -71,5 +215,18 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox clubIdTxtBox;
+        private System.Windows.Forms.TextBox clubNameTxtBox;
+        private System.Windows.Forms.TextBox clubAbbvTxtBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker foundDate;
+        private System.Windows.Forms.TextBox clubPassTxtBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox clubCityTxtBox;
+        private System.Windows.Forms.Label label7;
     }
 }
--- Football Scouting System/FA/AddClubForm.cs
+++ Football Scouting System/FA/AddClubForm.cs
@@ -14,17 +14,111 @@ namespace Football_Scouting_System.FA
     {
         private FAHomeScreen ParentForm_;
         Controller controllerobj;
+        DataTable FA_Leagues;
 
         public AddClubForm(FAHomeScreen _ParentForm)
         {
             InitializeComponent();
             ParentForm_ = _ParentForm;
             controllerobj = new Controller();
+            UpdateComboBoxes();
         }
 
         private void AddClubForm_FormClosed(object sender, FormClosedEventArgs e)
         {
             ParentForm_.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int ID = GetClubId();
+                string Name = GetClubName();
+                string Abbv = GetClubAbbv();
+                string City = GetClubCity();
+                string LeagueName = Convert.ToString(comboBox1.SelectedValue);
+                DateTime FoundDate = GetClubFD();
+                string Pass = GetClubPass();
+                controllerobj.insertClub(ID, Name, Abbv, City, LeagueName, ParentForm_.GetFA_ID(), FoundDate, Pass);
+                MessageBox.Show("Club added!");
+                this.Close();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //To get Club data and validate it
+        private int GetClubId()
+        {
+            try
+            {
+                int id = Convert.ToInt32(clubIdTxtBox.Text);
+                return id;
+            }
+            catch
+            {
+                throw new System.Exception("Please make sure you entered numbers only in ID!");
+            }
+        }
+
+        private string GetClubName()
+        {
+                string Name = clubNameTxtBox.Text;
+                if (Name == "")
+                    throw new System.Exception("Please enter the club name!");
+            for (int i = 0; i < Name.Length; ++i)
+            {
+                if (Name[i] >= '0' && Name[i] <= '9')
+                    throw new System.Exception("Please don't enter numbers in the club name!");
+            }
+            return Name;
+        }
+
+        private string GetClubAbbv()
+        {
+            string abbv = clubAbbvTxtBox.Text;
+            if (abbv == "")
+                throw new System.Exception("Please enter the club abbreviation!");
+            return abbv;
+        }
+
+        private string GetClubCity()
+        {
+            string city = clubCityTxtBox.Text;
+            for(int i = 0; i < city.Length; ++i)
+            {
+                if (city[i] >= '0' && city[i] <= '9')
+                    throw new System.Exception("Please don't enter numbers in the city name!");
+            }
+            return city;
+        }
+
+        private DateTime GetClubFD()
+        {
+            DateTime fd = foundDate.Value.Date;
+            return fd;
+        }
+
+        private string GetClubPass()
+        {
+            string pass = clubPassTxtBox.Text;
+            if (pass.Length <= 6)
+                throw new System.Exception("The club's password must be more than 6 characters!");
+            //TODO: Should add hashing here
+            return pass;
+        }
+
+        //To update comboboxes
+        private void UpdateComboBoxes()
+        {
+            FA_Leagues = controllerobj.GetLeagues(ParentForm_.GetFA_ID());
+            comboBox1.DataSource = FA_Leagues;
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "Name";
+            comboBox1.SelectedItem = null;
         }
     }
 }
