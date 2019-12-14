e1b41a8a882e1032f89800b1f72371b2253fd286
Omar Hadhoud <OmarHadhoud@ymail.com>
2019-12-14 14:13:40 +0200

    Fixed selecting multiple rows when accepting/rejecting offer.

--- Football Scouting System/FA/SigningsForm.Designer.cs
+++ Football Scouting System/FA/SigningsForm.Designer.cs
@@ -40,9 +40,11 @@
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(65, 34);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(1051, 394);
             this.dataGridView1.TabIndex = 0;
             // 
