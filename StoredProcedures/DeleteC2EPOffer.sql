3cb794eb0728f96d995e51c65513d4bd895cf58d
Omar Salem Hadhoud <omarhadhoud@ymail.com>
2019-12-14 12:43:16 +0200

    Updated some functions.

---
+++ StoredProcedures/DeleteC2EPOffer.sql
@@ -0,0 +1,29 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE DeleteC2EPOffer 
+	@FA_ID int,
+	@SCID int,
+	@PID int
+AS
+BEGIN
+	Delete from OfferPlayerC2E where EFAID = @FA_ID and SenderClubID = @SCID and PlayerID = @PID
+END
+GO
